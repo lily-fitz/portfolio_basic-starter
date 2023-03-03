c9f78387712f790d8783083d3d1756c23d2aad06
Lily Fitz <lilymfitz@gmail.com>
2023-03-03 13:08:17 -0500

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+// Variable: Mobile menu button
+// Variable: Navigation list
+
+////////////// Goal: When the button is clicked, we display the navigation menu
+
+// We want the button to listen for a click event
+
+//// Variable: The navigation list is/isn't visible
+//// Conditional: When we click the button:
+////   If the menu opens, we set the variable to true (is visible).
+////   If the menu closes, we set the variable to false (isn't visible).
+
+////////////// Goal: When Esc is pressed, we close the navigation menu
+
+// We want the button to listen for a key press event
+//// Variable: The Escape key has been pressed (check the key pressed equals the ESC key)
+//// Variable: The navigation list is/isn't visible
+
+//// Conditional: IF the navigation menu AND the key pressed was escape, hide the navigation menu, switch the toggle button from an 'X' back to a hamburger
